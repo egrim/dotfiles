b6f6078d005228f4927fdd0eb427f74249d3c6d5
Evan Grim <evan@mirgnave.com>
2013-06-01 16:48:43 -0500

    Add some mathias options that look useful

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -5,6 +5,16 @@ shopt -s nocaseglob
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
 
+# Autocorrect typos in path names when using `cd`
+shopt -s cdspell
+
+# Enable some Bash 4 features when possible:
+# * `autocd`, e.g. `**/qux` will enter `./foo/bar/baz/qux`
+# * Recursive globbing, e.g. `echo **/*.txt`
+for option in autocd globstar; do
+    shopt -s "$option" 2> /dev/null
+done
+
 export GREP_OPTIONS='--color=auto'
 
 # Prevent less from clearing the screen while still showing colors.
