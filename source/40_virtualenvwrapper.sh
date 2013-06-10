b9e5a0db6767342c6084aafb82c109407d8a892d
Evan Grim <evan@mirgnave.com>
2013-06-10 20:46:30 +0000

    Add venv-burrito support

--- source/40_virtualenvwrapper.sh
+++ source/40_virtualenvwrapper.sh
@@ -1,3 +1,8 @@
 if [ -r /usr/local/bin/virtualenvwrapper.sh ]; then
     source /usr/local/bin/virtualenvwrapper.sh
 fi
+
+# startup virtualenv-burrito
+if [ -f $HOME/.venvburrito/startup.sh ]; then
+    . $HOME/.venvburrito/startup.sh
+fi
