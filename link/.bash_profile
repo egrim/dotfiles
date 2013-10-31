44456f759a9687397f228f17d09cede690381e37
Evan Grim <evan@mirgnave.com>
2013-10-31 10:58:16 -0500

    Update virtualenv setup to match what burrito does

--- link/.bash_profile
+++ link/.bash_profile
@@ -1,3 +1,8 @@
 if [ -f ~/.bashrc ]; then
   source ~/.bashrc
 fi
+
+# startup virtualenv-burrito
+if [ -f $HOME/.venvburrito/startup.sh ]; then
+    . $HOME/.venvburrito/startup.sh
+fi
--- source/40_virtualenvwrapper.sh
+++
@@ -1,8 +0,0 @@
-if [ -r /usr/local/bin/virtualenvwrapper.sh ]; then
-    source /usr/local/bin/virtualenvwrapper.sh
-fi
-
-# startup virtualenv-burrito
-if [ -f $HOME/.venvburrito/startup.sh ]; then
-    . $HOME/.venvburrito/startup.sh
-fi
