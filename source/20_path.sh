14e0094d81cf6a44a1cc5e2e6fb303a1081d5a8d
Evan Grim <evan@mirgnave.com>
2018-09-27 12:05:38 -0500

    Switch to rbenv (nuke rvm)

--- source/20_path.sh
+++ source/20_path.sh
@@ -3,7 +3,6 @@ PATH=$(path_remove ~/opt/google_appengine):~/opt/google_appengine
 PATH=$(path_remove ~/android-sdks/platform-tools):~/android-sdks/platform-tools
 PATH=$(path_remove /usr/local/sbin):/usr/local/sbin
 PATH=$(path_remove /usr/local/heroku/bin):/usr/local/heroku/bin
-PATH=$(path_remove $HOME/.rvm/bin):$HOME/.rvm/bin
 PATH=$(path_remove $(npm -g bin 2> /dev/null)):$(npm -g bin 2> /dev/null)
 
 export PATH
